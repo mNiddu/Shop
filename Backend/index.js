8d59e1eaee71a5089adb1c5f3d21d3fc63fe05f4
mNiddu <mniddu19@gmail.com>
2024-03-09 17:41:37 +0530

    second commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,10 @@
 const ConnectToMongo = require('./db');
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app=express();
  app.use(express.json())
-
+app.use(cors());
  ConnectToMongo();
 app.use('/api/admin',require('./router/login'))
 const port=process.env.PORT
